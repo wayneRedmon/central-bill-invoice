89db7c7773c8a1c39be3f7afdc2c5cfe66bd77c5
wayneredmon <wredmon@prairiefarms.com>
2023-02-13 09:20:01 -0500

    refactored as final static class

--- src/main/java/com/prairiefarms/billing/utils/FolderMaintenance.java
+++ src/main/java/com/prairiefarms/billing/utils/FolderMaintenance.java
@@ -1,6 +1,5 @@
 package com.prairiefarms.billing.utils;
 
-import com.prairiefarms.billing.BillingEnvironment;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.io.File;
@@ -12,22 +11,24 @@ public final class FolderMaintenance {
 
     private static final long TWO_WEEK_PURGE_TIME = System.currentTimeMillis() - (14L * 24 * 60 * 60 * 1000);
 
-    public static void clean() throws Exception {
-        final File directory = new File(BillingEnvironment.getInstance().emailSentBox());
+    public static void clean(String pathAsString) throws Exception {
+        final File directory = new File(pathAsString);
 
         if (directory.exists()) {
             final File[] files = directory.listFiles();
 
             if (ObjectUtils.isNotEmpty(files)) {
                 for (File file : files) {
-                    if (file.lastModified() >= TWO_WEEK_PURGE_TIME && !file.delete())
-                        throw new Exception("Exception occurred while deleting file " + file.getPath());
+                    if (file.lastModified() >= TWO_WEEK_PURGE_TIME) {
+                        if (!file.delete())
+                            throw new Exception("Exception occurred while deleting file " + file.getPath());
+                    }
                 }
             }
         }
     }
 
-    public static void move(String from, String to) throws IOException {
+    public static void move(final String from, final String to) throws IOException {
         Files.move(Paths.get(from), Paths.get(to));
     }
 }
